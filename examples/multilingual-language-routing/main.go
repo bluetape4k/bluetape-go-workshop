dfe81a111cf80076a9094bae450a36ceba67155e
debop <sunghyouk.bae@gmail.com>
2026-07-12 22:21:35 +0900

    feat: add language routing preview command

---
+++ examples/multilingual-language-routing/main.go
@@ -0,0 +1,50 @@
+// Package main prints the multilingual language routing preview.
+package main
+
+import (
+	"encoding/json"
+	"errors"
+	"flag"
+	"fmt"
+	"io"
+	"os"
+
+	"github.com/bluetape4k/bluetape-go-workshop/examples/multilingual-language-routing/internal/routing"
+)
+
+func main() {
+	os.Exit(run(os.Args[1:], os.Stdout, os.Stderr))
+}
+
+func run(args []string, stdout, stderr io.Writer) int {
+	flags := flag.NewFlagSet("multilingual-language-routing", flag.ContinueOnError)
+	flags.SetOutput(stderr)
+	flags.Usage = func() {
+		fmt.Fprintln(stderr, "Usage: multilingual-language-routing [--preload]")
+		flags.PrintDefaults()
+	}
+	preload := flags.Bool("preload", false, "preload selected language models during router construction")
+	if err := flags.Parse(args); err != nil {
+		if errors.Is(err, flag.ErrHelp) {
+			return 0
+		}
+		return 2
+	}
+	if flags.NArg() != 0 {
+		fmt.Fprintf(stderr, "unexpected argument: %q\n", flags.Arg(0))
+		return 2
+	}
+
+	preview, err := routing.NewPreview(*preload)
+	if err != nil {
+		fmt.Fprintf(stderr, "build multilingual language routing preview: %v\n", err)
+		return 1
+	}
+	encoder := json.NewEncoder(stdout)
+	encoder.SetIndent("", "  ")
+	if err := encoder.Encode(preview); err != nil {
+		fmt.Fprintf(stderr, "encode multilingual language routing preview: %v\n", err)
+		return 1
+	}
+	return 0
+}
